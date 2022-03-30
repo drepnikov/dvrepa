fdeb5fabfb8401249fc6d4f543dd17def833327b
dvrepa <d.v.repnikov@gmail.com>
2022-03-30 21:25:03 +0300

    Секция приветствия

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import { Navigation } from "components/Navigation/Navigation";
+import css from "styles/Home.module.scss";
 
 const Home: NextPage = () => {
   return (
@@ -8,10 +9,22 @@ const Home: NextPage = () => {
       <Head>
         <title>d.v.repnikov</title>
       </Head>
-      <header>
+      <header className={css.header}>
         <Navigation />
       </header>
-      <main>Контент</main>
+      <main>
+        <section className={css.helloContainer}>
+          <div>
+            <h1 className={css.helloContainer__fio}>Репников.Дмитрий</h1>
+            <p className={css.helloContainer__description}>
+              Frontend-разработчик
+            </p>
+            <a className={css.helloContainer__btn} href={"#contacts"}>
+              Поздороваться
+            </a>
+          </div>
+        </section>
+      </main>
       <footer>Футеры</footer>
     </>
   );
