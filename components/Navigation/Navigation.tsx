2a533a9d951323edf5ad30b1068c72c80c8b23b5
dvrepa <d.v.repnikov@gmail.com>
2022-03-30 15:02:15 +0300

    Размечаю сайт

---
+++ components/Navigation/Navigation.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+import css from "./Navigation.module.scss";
+
+interface INavigationProps {}
+
+const Navigation: React.FC<INavigationProps> = () => {
+  return (
+    <nav className={css.navigation}>
+      <ul>
+        <li>
+          <a href="#about">Обо мне</a>
+        </li>
+        <li>
+          <a href="#contacts">Контакты</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export { Navigation };
