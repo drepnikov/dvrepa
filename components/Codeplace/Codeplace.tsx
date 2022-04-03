08a7c35f0e047615d00996388007ec4429fc5819
dvrepa <d.v.repnikov@gmail.com>
2022-04-03 18:35:21 +0300

    Разрабатываю компонкент Codeplace

--- components/Codeplace/Codeplace.tsx
+++ components/Codeplace/Codeplace.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import {useCodeplaceStyles} from "components/Codeplace/styles/useCodeplaceStyles";
+import { useCodeplaceStyles } from "components/Codeplace/styles/useCodeplaceStyles";
 
 interface ICodePlaceProps {
   stringToCode: string;
@@ -7,8 +7,24 @@ interface ICodePlaceProps {
 
 const CodePlace: React.FC<ICodePlaceProps> = ({ stringToCode }) => {
   const s = useCodeplaceStyles();
-  
-  return <div>{stringToCode}</div>;
+
+  return (
+    <div className={s.container}>
+      <div className={s.panel}>
+        <div className={s.panelButtons}>
+          <span className={s.windowCloseBtn} />
+          <span className={s.windowMinimazeBtn} />
+          <span className={s.windowFullScreenBtn} />
+        </div>
+        <div className={s.fileTabsContainer}>
+          <div className={s.fileTab}>
+            <span>index.js</span>
+          </div>
+        </div>
+      </div>
+      {stringToCode}
+    </div>
+  );
 };
 
 export { CodePlace };
--- components/Codeplace/styles/useCodeplaceStyles.ts
+++ components/Codeplace/styles/useCodeplaceStyles.ts
@@ -1,9 +1,19 @@
 import { useMemo } from "react";
 import css from "./Codeplace.module.scss";
+import classNames from "classnames";
 
 const useCodeplaceStyles = () => {
   return useMemo(() => {
-    return {};
+    return {
+      container: classNames(css.container),
+      panel: classNames(css.panel),
+      panelButtons: classNames(css.panelButtons),
+      fileTabsContainer: classNames(css.fileTabsContainer),
+      fileTab: classNames(css.fileTab),
+      windowCloseBtn: classNames(css.windowBtn, css.windowCloseBtn),
+      windowFullScreenBtn: classNames(css.windowBtn, css.windowFullScreenBtn),
+      windowMinimazeBtn: classNames(css.windowBtn, css.windowMinimazeBtn),
+    };
   }, []);
 };
 
