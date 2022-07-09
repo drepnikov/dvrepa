25867f6ca1eaca803095a0636772ad6a2a9c7b47
dvrepa <d.v.repnikov@gmail.com>
2022-07-09 18:13:51 +0300

    Что-то с чем-то

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,50 +5,84 @@ import { MyLink } from "components/MyLink/MyLink";
 import { CodePlace } from "components/Codeplace/Codeplace";
 
 const Home: NextPage = () => {
-  const s = useHomePageStyles();
+    const s = useHomePageStyles();
 
-  return (
-    <PageContainer>
-      <main>
-        <section className={s.helloContainer}>
-          <div>
-            <h1 className={s.fio}>Репников.Дмитрий</h1>
-            <div className={s.codePlaceContainer}>
-              <CodePlace stringToCode={"Frontend-Разработчик"} />
-            </div>
-            <p className={s.sayHiContainer}>
-              <MyLink
-                to={"/#contacts"}
-                title={"Поздороваться"}
-                className={s.sayHiBtn}
-              />
-            </p>
-          </div>
-          <div className={s.logos}>
-            <div className={s.firstRowLogoContainer}>
-              <object
-                className={s.jsLogo}
-                type={"image/svg+xml"}
-                data={"/js_logo.svg"}
-              />
-            </div>
-            <div>
-              <object
-                className={s.tsLogo}
-                type={"image/svg+xml"}
-                data={"/ts_logo.svg"}
-              />
-              <object
-                className={s.reactLogo}
-                type={"image/svg+xml"}
-                data={"/react_logo.svg"}
-              />
-            </div>
-          </div>
-        </section>
-      </main>
-    </PageContainer>
-  );
+    return (
+        <PageContainer>
+            <main>
+                {/* Основное */}
+                <section className={s.helloContainer}>
+                    <div>
+                        <h1 className={s.fio}>Репников.Дмитрий</h1>
+                        <div className={s.codePlaceContainer}>
+                            <CodePlace stringToCode={"Frontend-Разработчик"} />
+                        </div>
+                        <p className={s.sayHiContainer}>
+                            <MyLink
+                                to={"/#contacts"}
+                                title={"Поздороваться"}
+                                className={s.sayHiBtn}
+                            />
+                        </p>
+                    </div>
+                    <div className={s.logos}>
+                        <div className={s.firstRowLogoContainer}>
+                            <object
+                                className={s.jsLogo}
+                                type={"image/svg+xml"}
+                                data={"/js_logo.svg"}
+                            />
+                        </div>
+                        <div>
+                            <object
+                                className={s.tsLogo}
+                                type={"image/svg+xml"}
+                                data={"/ts_logo.svg"}
+                            />
+                            <object
+                                className={s.reactLogo}
+                                type={"image/svg+xml"}
+                                data={"/react_logo.svg"}
+                            />
+                        </div>
+                    </div>
+                </section>
+
+                {/* Обо мне */}
+                <section className={s.aboutMeContainer}>
+                    <div>
+                        <div className={s.aboutMeArticle}>
+                            Why do we use it? It is a long established fact that
+                            a reader will be distracted by the readable content
+                            of a page when looking at its layout. The point of
+                            using Lorem Ipsum is that it has a more-or-less
+                            normal distribution of letters, as opposed to using
+                            'Content here, content here', making it look like
+                            readable English. Many desktop publishing
+                        </div>
+                        <div className={s.aboutMeShortInfo}>
+                            <div className={s.photoAndAbout}>
+                                <img src="/me.jpg" className={s.myPhoto} />
+                                <div className={s.about}>
+                                    <div>
+                                        <div>Репников Дмитрий</div>
+                                        <div>Frontend-разрабочик</div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className={s.quoteOfGreatest}>
+                                Цитата великого
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
+                {/* Контакты */}
+                <section></section>
+            </main>
+        </PageContainer>
+    );
 };
 
 export default Home;
--- styles/Home/useHomePageStyles.ts
+++ styles/Home/useHomePageStyles.ts
@@ -1,23 +1,12 @@
 import { useMemo } from "react";
-import classNames from "classnames";
 import css from "./Home.module.scss";
 
-const useHomePageStyles = () => {
-  return useMemo(() => {
-    return {
-      helloContainer: classNames(css.helloContainer),
-      header: classNames(css.header),
-      fio: classNames(css.fio),
-      sayHiBtn: classNames(css.sayHiBtn),
-      logos: classNames(css.logos),
-      firstRowLogoContainer: classNames(css.firstRowLogoContainer),
-      tsLogo: classNames(css.logo, css.tsLogo),
-      jsLogo: classNames(css.logo, css.jsLogo),
-      reactLogo: classNames(css.logo, css.reactLogo),
-      sayHiContainer: classNames(css.sayHiContainer),
-      codePlaceContainer: classNames(css.codePlaceContainer),
-    };
-  }, []);
+const useHomePageStyles = (): typeof css => {
+    return useMemo(() => {
+        return {
+            ...css,
+        };
+    }, []);
 };
 
 export { useHomePageStyles };
